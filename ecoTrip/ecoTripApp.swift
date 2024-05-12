6ea71433b1e542c11e9ff937f2ef8f796ead1efc
ichichang <ichichang0916@gmail.com>
2024-05-12 17:44:10 +0800

    first commit

---
+++ ecoTrip/ecoTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecoTripApp.swift
+//  ecoTrip
+//
+//  Created by Ichi Chang on 2024/5/12.
+//
+
+import SwiftUI
+
+@main
+struct ecoTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
